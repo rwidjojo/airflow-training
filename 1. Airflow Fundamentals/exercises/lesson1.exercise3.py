44b61701498d6466c7d5967314c0c4faf67ed75d
Nurcahyo Pujo N <nurcahyopujo@gmail.com>
2020-12-15 02:06:08 +0700

    courser: fundamental add exercise

---
+++ 1. Airflow Fundamentals/exercises/lesson1.exercise3.py
@@ -0,0 +1,35 @@
+#
+# Instructions: 
+# Change DAG schedule interval so DAG could automatically run every 30 minutes
+#
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+owner = 'john_doe' # Replace with your short name
+
+default_args = {
+    'owner': owner,
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
+dag = DAG(
+    f'{owner}.lesson1.excercise3',
+    default_args=default_args,
+    description='DAG simple pipeline',
+    # Place schedule interval here
+)
+
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
